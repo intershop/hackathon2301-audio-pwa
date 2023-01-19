ccc9c35975ec75e82a3146e9248433cc6f4bd59b
Andreas Steinmann <asteinmann@intershop.com>
2023-01-19 09:20:59 +0100

    feat: add daily report

--- src/app/shared/components/audio-text-to-speech/audio-text-to-speech.component.ts
+++ src/app/shared/components/audio-text-to-speech/audio-text-to-speech.component.ts
@@ -7,6 +7,7 @@ import { whenTruthy } from 'ish-core/utils/operators';
 @Component({
   selector: 'ish-audio-text-to-speech',
   templateUrl: './audio-text-to-speech.component.html',
+  styleUrls: ['./audio-text-to-speech.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AudioTextToSpeechComponent implements OnInit {
@@ -15,9 +16,16 @@ export class AudioTextToSpeechComponent implements OnInit {
   ngOnInit() {
     this.accountFacade.user$.pipe(whenTruthy(), take(1)).subscribe(user => {
       const utterance = new SpeechSynthesisUtterance(
-        `It's nice to see you again - ${user.firstName} ${user.lastName} !!!`
+        `Hello! It's nice to see you again - ${user.firstName} ${user.lastName} !!!`
       );
       speechSynthesis.speak(utterance);
     });
+  }
+
+  playDailyReport() {
+    const utterance = new SpeechSynthesisUtterance(
+      `This is your daily report: Budget is as usual. You have 6 requisitions which need to be approved.`
+    );
+    speechSynthesis.speak(utterance);
   }
 }
